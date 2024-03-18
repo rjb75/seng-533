7c858f443166b3816dcff381697d7cde0173e97a
Robert Brown <brown.rjb75@gmail.com>
2024-03-17 19:38:20 -0600

    fix: update ramps

--- stressTests/create-review.js
+++ stressTests/create-review.js
@@ -1,12 +1,17 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
+import http from "k6/http";
+import { sleep } from "k6";
 
 export const options = {
-  stages: [
-    { duration: '10m', target: 200 }, // traffic ramp-up from 1 to a higher 200 users over 10 minutes.
-    { duration: '30m', target: 200 }, // stay at higher 200 users for 30 minutes
-    { duration: '5m', target: 0 }, // ramp-down to 0 users
-  ],
+  scenarios: {
+    ramp: {
+      executor: "ramping-vus",
+      stages: [
+        { duration: "10m", target: 1000 },
+        { duration: "30m", target: 1000 },
+        { duration: "5m", target: 0 },
+      ],
+    },
+  },
 };
 
 // The function that defines VU logic.
@@ -14,26 +19,77 @@ export const options = {
 // See https://grafana.com/docs/k6/latest/examples/get-started-with-k6/ to learn more
 // about authoring k6 scripts.
 //
-export default function() {
+export default function () {
+  const tags = [
+    "good",
+    "bad",
+    "tasty",
+    "filling",
+    "interesting",
+    "boring",
+    "quick",
+    "long",
+    "walking",
+    "outdoors",
+    "indoors",
+    "snack",
+    "lunch",
+    "dinner",
+  ];
+
+  const titles = [
+    "Great Snack",
+    "Quick Lunch",
+    "Dinner",
+    "Late Night Snack",
+    "Early Morning Breakfast",
+    "Lunch",
+    "Dinner",
+    "Late Night Snack",
+    "Early Morning Breakfast",
+    "Lunch",
+    "Dinner",
+    "Late Night Snack",
+    "Early Morning Breakfast",
+  ]
+
+  const descriptions = [
+    "Stopped in for a quick snack and it did not disappoint",
+    "The food was great and the service was even better",
+    "I was in and out in no time",
+    "A unique take on my favorite dishes",
+    "Friendly staff and great food",
+  ]
 
   const url = "http://test.xplored:8000/api/v1/review/";
   const params = {
     headers: {
-      'Authorization': '8d7ef110325a18c5267a72af04449fffa471291faddce844bc4c3c0481034de7',
-      'X-User': 'nbq52RSCA2NLfkNc77Kkl4e2ot03',
+      Authorization:
+        "8d7ef110325a18c5267a72af04449fffa471291faddce844bc4c3c0481034de7",
+      "X-User": "nbq52RSCA2NLfkNc77Kkl4e2ot03",
     },
   };
-  
+
+  // select 3 random tags
+  const randomTags = [];
+  for (let i = 0; i < 3; i++) {
+    randomTags.push(tags[Math.floor(Math.random() * tags.length)]);
+  }
+
+  const rating = Math.floor(Math.random() * 5) + 1;
+
+  const title = titles[Math.floor(Math.random() * titles.length)];
+  const description = descriptions[Math.floor(Math.random() * descriptions.length)];
+
   const body = {
     first_name: "Robert",
     last_name: "Brown",
-    title: "A Tasty Quick Byte",
-    stars: "3",
-    tags: ["food", "snack", "quick"],
-    description: "Stopped in for a quick snack and it did not disappoint",
+    title: title,
+    stars: `${rating}`,
+    tags: randomTags,
+    description: description,
   };
-
 
   http.post(url, body, params);
   sleep(1);
-};
+}
