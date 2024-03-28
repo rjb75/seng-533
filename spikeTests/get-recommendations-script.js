aa63cb699a86af0569ceb6ecd53014e1efcf4033
Robert Brown <brown.rjb75@gmail.com>
2024-03-27 20:29:59 -0600

    feat: spike tests

---
+++ spikeTests/get-recommendations-script.js
@@ -0,0 +1,44 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    scenarios: {
+      ramp: {
+        executor: "ramping-vus",
+        stages: [
+          { duration: "2m", target: 2000 },
+          { duration: "1m", target: 0 },
+        ],
+      },
+    },
+  };
+
+export default function() {
+
+    const locations = [
+        "Canada",
+        "Malta",
+        "Australia",
+        "Japan",
+        "France",
+        "Brazil",
+        "South%20Africa",
+        "Italy",
+        "Thailand",
+        "United%20Arab%20Emirates",
+    ];
+
+    const location = locations[Math.floor(Math.random() * locations.length)];
+
+    const url = `http://test.xplored:8000/api/v1/recommendations?location_name=${location}`;
+    const params = {
+        headers: {
+            'Authorization': '8d7ef110325a18c5267a72af04449fffa471291faddce844bc4c3c0481034de7',
+            'X-User': 'nbq52RSCA2NLfkNc77Kkl4e2ot03',
+        },
+    };
+
+
+    http.get(url, params);
+    sleep(1);
+}
--- stress-runner.sh
+++ stress-runner.sh
@@ -3,6 +3,6 @@
 # for each js file in the smokeTests directory
 
 for file in stressTests/*.js; do
-  k6 run $file --summary-export=results/$(basename $file .js).json --out csv=results/stress-$(basename $file .js).csv
+  k6 run $file --summary-export=results/stress-$(basename $file .js).json --out csv=results/stress-$(basename $file .js).csv
   echo "Ran $file" >> results/stress-summary.txt
 done
